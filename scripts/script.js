6bc00b5cb8301fc8579484a5cd18841b1e010818
Erik Saarlas <erikw.saarlas@gmail.com>
2024-02-28 17:29:06 -0600

    trying to get button working

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.section-container');
-    const showSectionsBtn = document.querySelector('.show-sections-btn'); 
+  const sections = document.querySelectorAll('.section-container');
+  const showSectionsBtn = document.querySelector('.show-sections-btn');
 
-    showSectionsBtn.addEventListener('click', () => {
-        let delay = 0;
-        sections.forEach(section => {
-            setTimeout(() => {
-                section.classList.add('active');
-            }, delay);
-            delay += 150; 
-        });
+  showSectionsBtn.addEventListener('click', () => {
+    let delay = 0;
+    sections.forEach(section => {
+      setTimeout(() => {
+        section.classList.add('active');
+      }, delay);
+      delay += 150;
     });
+  });
 });
