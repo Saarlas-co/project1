2b0322e52fae1db22f22edecdc53c2ce2a1df055
Erik Saarlas <erikw.saarlas@gmail.com>
2024-02-28 19:09:50 -0600

    changed CSS and JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
     document.getElementById('exploreButton').addEventListener('click', function() {
         var mainContent = document.getElementById('mainContent');
+        mainContent.style.display = 'block'; =
         mainContent.scrollIntoView({ behavior: 'smooth' });
     });
 
-    
     var signupForm = document.getElementById('signupForm');
     signupForm.addEventListener('submit', function(event) {
-       
-        event.preventDefault(); 
-        console.log("form submitted");
+        event.preventDefault(); =
+        console.log("Form submitted");
+
+        =
     });
 });
