23604653621d052f3aafacc5fa066d133bb65752
Erik Saarlas <erikw.saarlas@gmail.com>
2024-02-28 18:26:36 -0600

    new website design. experimenting with scroll effect js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var exploreButton = document.getElementById('exploreButton');
-    exploreButton.addEventListener('click', function() {
+    
+    document.getElementById('exploreButton').addEventListener('click', function() {
         var mainContent = document.getElementById('mainContent');
-        mainContent.style.display = mainContent.style.display === 'none' ? 'block' : 'none';
+        /
+        mainContent.classList.add('visible'); 
     });
 
+    
     var signupForm = document.getElementById('signupForm');
     signupForm.addEventListener('submit', function(event) {
+       
         event.preventDefault(); 
         console.log("form submitted");
     });
